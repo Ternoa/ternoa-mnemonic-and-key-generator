4610762856937e268f33c4683e7ef4164342a92b
Mickael <mickael@netick.fr>
2019-09-21 20:59:36 +0200

    prepare script to generate

--- demo.js
+++ demo.js
@@ -6,16 +6,16 @@ const mnemonic = 'yesterday once more happy bride smile short lovers make life s
 const password = 'this is a totally long password';
 
 console.log('mnemonic               : %s', mnemonic);
-console.log('password               : %s', password);
+//console.log('password               : %s', password);
 
 const privateKeyGen = pkutils.getPrivateKeyFromMnemonic(mnemonic);
 console.log('pkey from mnemonic     : 0x%s', privateKeyGen);
 
 const keystore = pkutils.getKeystoreFromPrivateKey(privateKeyGen, password);
-console.log('\nkey store              : %j\n', keystore);
+//console.log('\nkey store              : %j\n', keystore);
 
 const privateKeyParsed = pkutils.getPrivateKeyFromKeystore(keystore, password);
 console.log('pkey from keystore     : 0x%s', privateKeyParsed);
 
 const account = keystore.address;
-console.log('account address        : 0x%s', account);
+//console.log('account address        : 0x%s', account);
